34565aba6a41571a2bfb82dc514861bb7b6a1cd5
StivenDeveloper <evertstiven619@gmail.com>
2022-12-22 16:36:31 -0500

    clase 20: evento de javascript addEventListener

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,24 @@
-function seleccionarMascotaJugador(){
-    alert("Seleccionarte tu mascota");
+
+function iniciarJuego(){
+
+    let botonMascotaJugador = document.getElementById("boton-mascota");
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+
+    
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota");
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+function seleccionarMascotaJugador(){
+
+    if (document.getElementById("hipodoge").checked){
+        alert("Seleccionaste a Hipodoge");
+    }else if (document.getElementById("capipepo").checked){
+        alert("Seleccionaste a Capipepo");
+    }else if (document.getElementById("ratigueya").checked){
+        alert("seleccionaste a Ratigueya");
+    } else{
+        alert("Selecciona a una mascota para el ataque");
+    }
+    
+}
+
+window.addEventListener("load",iniciarJuego); 
