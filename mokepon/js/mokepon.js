444f2c3ffea1c3af3a0499e401c05f6949c6edcc
StivenDeveloper <evertstiven619@gmail.com>
2023-01-16 17:53:56 -0500

    Clase: 53: Una sola fuente de la verdad. OJO Debemos solucionar el tema de la elección aleatoria del enemigo en base a la elección del personaje

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -6,9 +6,7 @@ const botonAgua = document.getElementById("boton-agua");
 const botonHacha = document.getElementById("boton-hacha");
 const botanReiniciar = document.getElementById("boton-reiniciar")
 const sectionPersonaje = document.getElementById("seleccionar-personaje");
-const inputMario = document.getElementById("mario");
-const inputLuigi = document.getElementById("luigi");
-const inputBowser = document.getElementById("bowser");
+
 const spanPersonajeJugador = document.getElementById("personaje-jugador");
 
 const spanPersonajeEnemigo = document.getElementById("personaje-enemigo");
@@ -19,6 +17,10 @@ const sectionMensaje = document.getElementById("resultado");
 const ataqueDelJugador = document.getElementById("ataque-del-jugador");
 const ataqueDelEnemigo = document.getElementById("ataque-del-enemigo");
 
+const contenedorTarjetas = document.getElementById("contenedor-tarjetas")
+
+
+
 //Array, una estructura de dato estático, que nos permite agrupar valores en una sola variable
 
 let personajes = []
@@ -26,6 +28,10 @@ let personajes = []
 let ataqueJugador; //Se declararon variables globales para que se invoquen en cualquir función.
 let ataqueEnemigo;
 let PersonajeJugador;
+let opcionDePersonajes;
+let inputLuigi;
+let inputMario; 
+let inputBowser; 
 let vidasJugador= 3;
 let vidasEnemigo= 3;
 
@@ -35,8 +41,16 @@ class Personaje {
         this.nombre = nombre;
         this.foto = foto;
         this.vida = vida;
+        this.ataques = [];
     }
 }
+
+//                CREANDO CÓDIGO  HTML CON JAVASCRIPT (CLASE 50)
+// El término “renderizar” hace referencia al proceso que lleva a cabo el 
+// navegador para leer el código HTML y CSS para posteriormente dibujarlo en 
+// el navegador. Con Javascript, puedes crear código HTML de forma dinámica y 
+// lograr que el navegador muestre esos datos dentro del DOM de una página web.
+// Una vez tengas los datos preparados en tu código Javascript //
 
 let mario = new Personaje('Mario','./assets/pwd.png',5)
 
@@ -44,13 +58,66 @@ let luigi = new Personaje('Luigi','./assets/Luigi.png',5)
 
 let bowser = new Personaje('Bowser','./assets/Bowser_Stock_Art_2021.png',5)
 
-personajes.push(mario,luigi,bowser)
+mario.ataques.push(
+    {nombre: '🔥', id:'boton-fuego'},
+    {nombre: '🔥', id:'boton-fuego'},
+    {nombre: '🔥', id:'boton-fuego'},
+    {nombre: '💧', id:'boton-agua'},
+    {nombre: '🪓', id:'boton-hacha'},
+)
 
-console.log(personajes)
+luigi.ataques.push(
+    {nombre: '💧', id:'boton-agua'},
+    {nombre: '💧', id:'boton-agua'},
+    {nombre: '💧', id:'boton-agua'},
+    {nombre: '🔥', id:'boton-fuego'},
+    {nombre: '🪓', id:'boton-hacha'},
+)
+
+mario.ataques.push(
+    {nombre: '🪓', id:'boton-hacha'},
+    {nombre: '🪓', id:'boton-hacha'},
+    {nombre: '🪓', id:'boton-hacha'},
+    {nombre: '🔥', id:'boton-fuego'},
+    {nombre: '💧', id:'boton-agua'},
+)
+
+personajes.push(mario,luigi,bowser);
+
+
 
 function iniciarJuego(){
     sectionAtaque.style.display = "none";
     sectionReiniciar.style.display = "none";
+
+    // (Clase 50) A continuación, captura el elemento con document.getElementById(). Utilizaremos una nueva    función        llamada forEach() que todos los arreglos poseen para poder iterar cada elemento del mismo.   El    término “iterar” hace referencia a recorrer los elementos de un array, uno por uno en el mismo orden en el que    se  //encuentran.
+
+    // Observa que dentro del forEach(), estamos declarando una función que recibe como parámetro una variable en singular llamada Personaje. Aquí encontrarás el objeto con sus atributos y la información que renderizaremos al HTML.
+
+    // Dentro de esta función que se ejecutará una vez por cada iteración, estamos usando las comillas invertidas para escribir código HTML como si fuese texto simple. Este texto es procesado por Javascript y nos permite pasarle valores de variables a través de la sintaxis ${Personaje.nombre}.
+    
+    // De esta manera, puedes concatenar el código HTML generado por Javascript con el valor de las variables que necesites.
+    
+    // Finalmente, utiliza la propiedad innerHTML para insertar el código HTML en el DOM. Presta atención al += que nos permite concatenar cada iteración del array y que todos los datos se muestren correctamente.
+    
+    // Verás en el navegador web, los datos estructurados y renderizados desde el código Javascript.   
+    
+    personajes.forEach((Personaje)=>{
+        opcionDePersonajes = `
+        <input type="radio" name="personaje" id=${Personaje.nombre}>
+        <label id=${Personaje.nombre} class="tarjeta-de-personajes" for=${Personaje.nombre}>
+            <p>${Personaje.nombre}</p>
+            <img src=${Personaje.foto} alt=${Personaje.nombre}>
+        </label>
+        `
+    contenedorTarjetas.innerHTML += opcionDePersonajes; 
+
+        inputLuigi = document.getElementById("Luigi")
+        inputMario = document.getElementById("Mario")
+        inputBowser = document.getElementById("Bowser")
+
+        
+    })
     // El metodo getElementById nos permite hacer referencia a un elemento de html por medio de su ID
     botonPersonajeJugador.addEventListener("click", seleccionarPersonajeJugador);
     botonFuego.addEventListener("click",ataqueFuego)
@@ -64,14 +131,14 @@ function seleccionarPersonajeJugador(){
     sectionPersonaje.style.display = "none"
 
     if (inputMario.checked){
-        spanPersonajeJugador.innerHTML = "Mario";
-        PersonajeJugador = "Mario"
+        spanPersonajeJugador.innerHTML = inputMario.id;
+        PersonajeJugador = inputMario.id;
     }else if (inputLuigi.checked){
-        spanPersonajeJugador.innerHTML = "Luigi";
-        PersonajeJugador ="Luigi"
+        spanPersonajeJugador.innerHTML = inputLuigi.id;
+        PersonajeJugador = inputLuigi.id;
     }else if (inputBowser.checked){
-        spanPersonajeJugador.innerHTML = "Bowser";
-        PersonajeJugador = "Bowser"
+        spanPersonajeJugador.innerHTML = inputBowser.id;
+        PersonajeJugador = inputBowser.id;
     } else{
         alert("Selecciona a un personaje para el ataque");
         reiniciarJuego();
@@ -81,26 +148,24 @@ function seleccionarPersonajeJugador(){
 
 function seleccionarPersonajeEnemigo(){
     let jugadaEnemigo = 0;
-    if(PersonajeJugador=="Mario"){
-        jugadaEnemigo = Aleatorio(1,2)
+    jugadaEnemigo = Aleatorio(0,(personajes.length-2))
+    if(PersonajeJugador==inputMario.id){
         if(jugadaEnemigo==1){
-            spanPersonajeEnemigo.innerHTML = "Luigi";
+            spanPersonajeEnemigo.innerHTML = inputLuigi.id;
         }else{
-            spanPersonajeEnemigo.innerHTML = "Bowser";
+            spanPersonajeEnemigo.innerHTML = inputBowser.id;
         }
-    }else if(PersonajeJugador== "Luigi"){
-        jugadaEnemigo = Aleatorio(1,2);
+    }else if(PersonajeJugador== inputLuigi.id){
         if(jugadaEnemigo==1){
-            spanPersonajeEnemigo.innerHTML = "Mario";
+            spanPersonajeEnemigo.innerHTML = inputMario.id;
         }else{
-            spanPersonajeEnemigo.innerHTML = "Bowser";
+            spanPersonajeEnemigo.innerHTML = inputBowser.id;
     }
-    } else if(PersonajeJugador== "Bowser"){
-        jugadaEnemigo = Aleatorio(1,2);
+    } else if(PersonajeJugador== inputBowser.id){
         if(jugadaEnemigo==1){
-            spanPersonajeEnemigo.innerHTML = "Mario";
+            spanPersonajeEnemigo.innerHTML = inputMario.id;
         }else{
-            spanPersonajeEnemigo.innerHTML = "Luigi";
+            spanPersonajeEnemigo.innerHTML = inputLuigi.id;
         }
     }
 }
